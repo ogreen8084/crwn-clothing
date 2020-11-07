df840309b3b8f76b73fdc15cd17364121feadc13
Oscar Green <ogg@Oscars-MacBook-Pro-2.local>
2020-11-07 07:18:38 -0500

    Update with shop page

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
 import './App.css';
-import HomePage from './pages/homepage.component';
+import HomePage from './pages/homepage/homepage.component';
+import {Switch, Route} from 'react-router-dom';
+import ShopPage from './pages/shop/shop.components';
 
 function App() {
   return (
     <div>
-      <HomePage />
+    <Switch>
+      <Route exact path='/' component={HomePage} />
+      <Route path='/shop' component={ShopPage}/>
+      </Switch>
     </div>
   );
 }
--- src/components/menu-item/menu-item.components.jsx
+++ src/components/menu-item/menu-item.components.jsx
@@ -1,11 +1,11 @@
 import React from 'react';
 import './menu-item.styles.scss';
+import {withRouter} from 'react-router-dom';
 
-
-function MenuItem({title, imageUrl, size}) {
+function MenuItem({title, imageUrl, size, history, linkUrl, match}) {
     return (
         
-            <div className={`${size} menu-item`}>
+            <div className={`${size} menu-item`} onClick={() =>history.push(`${match.url}${linkUrl}`)}>
             <div className='background-image' style={{backgroundImage: `url(${imageUrl})`}} />
                 <div className='content'>
                     <h1 className='title'>{title.toUpperCase()}</h1>
@@ -15,4 +15,4 @@ function MenuItem({title, imageUrl, size}) {
             </div>
     )};
 
-export default MenuItem;
+export default withRouter(MenuItem);
